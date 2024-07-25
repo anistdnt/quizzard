887bcf28a720bd25920620a248835576529e4991
itzarkabrata <arkabrata1292@gmail.com>
2024-07-25 11:12:46 +0530

    fixed

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const home = require("./Rouer/Home/route");
 const startquiz = require("./Rouer/Startquiz/route");
 const exam = require("./Rouer/Examination/route");
 const port = process.env.PORT || 3000;
-const path = require('path')
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
