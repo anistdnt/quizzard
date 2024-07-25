0c573157ea0c044400ec0bcec0d1c1c57cb68c9e
itzarkabrata <arkabrata1292@gmail.com>
2024-07-25 22:30:16 +0530

    commited

--- Controler/Examination/controler.js
+++ Controler/Examination/controler.js
@@ -1,11 +1,46 @@
-const examFunc = (req,res)=>{
+const examFunc = (req, res) => {
     try {
-        res.render("examination");
+        res.render("examination", {
+            quizdata: [
+                {
+                    id: 1,
+                    qtype: "easy",
+                    domain: "technical",
+                    qname: "Javascript is ____ in nature?",
+                    options: ["Asynchronous", "Synchronous", "Multithreaded", "Single Threaded"],
+                    answer: "Asynchronous"
+                },
+                {
+                    id: 2,
+                    qtype: "intermediate",
+                    domain: "aptitude",
+                    qname: "Javascript is ____ in nature?",
+                    options: ["Asynchronous", "Synchronous", "Multithreaded", "Single Threaded"],
+                    answer: "Asynchronous"
+                },
+                {
+                    id: 3,
+                    qtype: "easy",
+                    domain: "political and history",
+                    qname: "Javascript is ____ in nature?",
+                    options: ["Asynchronous", "Synchronous", "Multithreaded", "Single Threaded"],
+                    answer: "Asynchronous"
+                },
+                {
+                    id: 4,
+                    qtype: "easy",
+                    domain: "geography",
+                    qname: "Javascript is ____ in nature?",
+                    options: ["Asynchronous", "Synchronous", "Multithreaded", "Single Threaded"],
+                    answer: "Asynchronous"
+                }
+            ]
+        });
     } catch (error) {
-        res.render("error",{
-            msg : error.message
+        res.render("error", {
+            msg: error.message
         })
     }
 }
 
-module.exports = {examFunc};
+module.exports = { examFunc };
