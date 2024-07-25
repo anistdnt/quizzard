780c860def9728920aadb11514ea5d60748ad41b
itzarkabrata <arkabrata1292@gmail.com>
2024-07-26 00:20:03 +0530

    js added

---
+++ Assets/js/script.js
@@ -0,0 +1,7 @@
+//Responsive navbar javascript 
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('.menu').classList.toggle('translate-x-full');
+});
+document.querySelector('.close').addEventListener('click', function() {
+    document.querySelector('.menu').classList.toggle('translate-x-full');
+});
